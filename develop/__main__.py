b10c08a76ef1fbf1ffd63437b7a58e6dfc61fe6a
pdonorio <tempo.pause@gmail.com>
2017-09-14 17:20:13 +0200

    cosmetic changes

--- develop/__main__.py
+++ develop/__main__.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import better_exceptions as be
-from invoke import Collection  # , Config
-from develop import __version__
 from develop.program import CLIProgram
+from develop import __version__
+from invoke import Collection
 from develop.mytasks import \
     package as package_ops, \
     release as release_ops
 
 log = CLIProgram.setup_logger()
 
-# class DoConfig(Config):
-#     prefix = 'cmd'
-#     # env_prefix = 'TEST'
-
-
-# Using namespaces for sub-sub commands
 ns = Collection()
 # ns.add_task()
 ns.add_collection(package_ops)
 ns.add_collection(release_ops)
+
 program = CLIProgram(namespace=ns, version=__version__)
-log.very_verbose("App: %s; w/ %s.", program, be.__name__)
+log.very_verbose("App: %s (+%s)", program, be.__name__)
