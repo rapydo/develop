d7c34f9d07da537c5a74fcb3f421ebcdc12c7597
pdonorio <tempo.pause@gmail.com>
2017-09-15 06:29:02 +0200

    Updating invoke

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     ],
     install_requires=[
         'rapydo-utils==%s' % current_version,
-        'invoke==0.19.0',
+        'invoke==0.20.4'
     ],
     entry_points={
         'console_scripts': [
