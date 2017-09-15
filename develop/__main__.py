0dfd57e4bec2527cc3878572fba6d9a05a984dd4
pdonorio <tempo.pause@gmail.com>
2017-09-15 06:25:55 +0200

    Move the program base to rapydo utilities

--- develop/__main__.py
+++ develop/__main__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import better_exceptions as be
-from develop.program import CLIProgram
+from utilities.cli import App
 from develop import __version__
 from invoke import Collection
 from develop.mytasks import \
     package as package_ops, \
     release as release_ops
 
-log = CLIProgram.setup_logger()
+log = App.setup_logger()
 
 ns = Collection()
 # ns.add_task()
 ns.add_collection(package_ops)
 ns.add_collection(release_ops)
 
-program = CLIProgram(namespace=ns, version=__version__)
+program = App(namespace=ns, version=__version__)
 log.very_verbose("App: %s (+%s)", program, be.__name__)
